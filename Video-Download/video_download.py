ea3b91054bb35bcca54ae52eb869e92233223ca8
PeiHsiuLu <144085514+PeiHsiuLu@users.noreply.github.com>
2024-04-28 13:04:51 +0800

    0428

---
+++ Video-Download/video_download.py
@@ -0,0 +1,5 @@
+from pytube import YouTube
+yt  = YouTube("https://www.youtube.com/watch?v=Udvz99WBEkw&list=RDUdvz99WBEkw&index=1")
+print("影片下載中，請等待")
+yt.streams.first().download()
+print("影片下載完成")
